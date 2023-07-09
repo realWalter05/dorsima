2e55722810d80a1ae06586aee61c7249158694c0
realWalter05 <zikavaclav05@gmail.com>
2023-07-09 15:08:31 +0200

    calculator

--- js/main.js
+++ js/main.js
@@ -23,14 +23,68 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+function StridacChoose(clicked) {
+    let deye = document.querySelector("#deye-check");
+    if (document.querySelector('label[for="deye-check"]').classList.contains("btn-dark")) {
+        let deyeLabel = document.querySelector('label[for="deye-check"]');
+        deyeLabel.classList.remove("btn-dark");
+        deyeLabel.classList.add("btn-outline-dark");
+    } 
+
+    let solax = document.querySelector("#solax-check");
+    if (document.querySelector('label[for="solax-check"]').classList.contains("btn-dark")) {
+        let solaxLabel = document.querySelector('label[for="solax-check"]');
+        solaxLabel.classList.remove("btn-dark");
+        solaxLabel.classList.add("btn-outline-dark");
+    } 
+
+    let solaredge = document.querySelector("#solaredge-check");
+    if (document.querySelector('label[for="solaredge-check"]').classList.contains("btn-dark")) {
+        let solaredgeLabel = document.querySelector('label[for="solaredge-check"]');
+        solaredgeLabel.classList.remove("btn-dark");
+        solaredgeLabel.classList.add("btn-outline-dark");
+    } 
+
+    clicked.classList.remove("btn-outline-dark");
+    clicked.classList.add("btn-dark");
+}
+
+function WriteSolarForm() {
+    let form = document.querySelector("#calculator-form");
+
+    let stridac = "";
+    if (form[1])
+        stridac = form[1].value;
+    if (form[2])
+        stridac = form[2].value;
+    if (form[3])
+        stridac = form[3].value; 
+
+    let message = "Mám zájem o " + form[0].value + " panelů, střídač značky " + stridac  + ", baterii s kapacitou " + form[4].value + " kWh. Vyřízení dotace: " + (form[5].checked ? "Ano" : "Ne") + ". Zajištění wallboxu: " + (form[6].checked ? "Ano" : "Ne") + ". Montáž optimizéru: " + (form[7].checked ? "Ano" : "Ne") + ".";
+    
+    let msgInput = document.querySelector("#message");
+    msgInput.value = message;
+    msgInput.classList.add("not-empty");
+
+    document.querySelector("#contact").scrollIntoView();
+}
+
 function CalculateSolar(solarForm, output, output_dotace) {
     console.log('recalculating');
     let totalPrice = 0;
 
     const panelCount = solarForm[0].value;
-    const vyriditDotaci = solarForm[1].checked;
-    const chceteWallbox = solarForm[2].checked;
-    const chceteOptimizer = solarForm[3].checked;
+
+    const deye = solarForm[1].checked;
+    const solax = solarForm[2].checked;
+    const solaredge = solarForm[3].checked;
+
+    const kwhBaterie = solarForm[4].value;
+
+    const vyriditDotaci = solarForm[5].checked;
+    const chceteWallbox = solarForm[6].checked;
+    const chceteOptimizer = solarForm[7].checked;
+
 
     if (vyriditDotaci) {
         totalPrice += 20000;
@@ -44,11 +98,22 @@ function CalculateSolar(solarForm, output, output_dotace) {
         totalPrice += 1200 * panelCount;
     }
 
-    // baterie 
-    // totalPrice += 450 * 5500;
+    // BATERIE 
+    totalPrice += kwhBaterie * 12000;
 
-    // naklady na panely
+    // PANELY
     totalPrice += panelCount * 13200;
+
+
+    // STRIDACE
+    if (deye)
+        totalPrice += 150000;
+    
+    if (solax)
+        totalPrice += 75000;
+
+    if (solaredge)
+        totalPrice += 100000;
 
     if (panelCount < 23) {
         // Stridac mensi price
@@ -58,21 +123,15 @@ function CalculateSolar(solarForm, output, output_dotace) {
         totalPrice += 455 * panelCount / 15000 * 75000;
     } 
 
-    // 150 000 Deye 
-    // 75 000 Solax
-    // 100 000 Solaredge
 
-    //  5 = 50 000
-
-
-    // elektroprace
+    // ELEKTROPRACE
     if (700 * panelCount < 15000) {
         totalPrice += 15000;
     } else {
         totalPrice += 700 * panelCount;
     }
 
-    // dalsi ceny
+    // OSTATNI (distributor, upravy, doprava, revize)
     totalPrice += 5000 + 3000 + 3000 + 5000 + 200;
     dotace = (totalPrice / 2) > 200000 ? 200000 : totalPrice /  2;
 
