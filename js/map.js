4c8564afa95c0463839c25467e6d02d47b2a32c1
realWalter05 <zikavaclav05@gmail.com>
2024-03-13 14:47:50 +0100

    map

--- js/map.js
+++ js/map.js
@@ -1,24 +1,11 @@
-async function eventMap() {
 
-await google.maps.event.addListener(map, 'zoom_changed', function () {
-    var zoomLevel = map.getZoom();
-    //DEBUG alert(zoomLevel+', '+map.getMapTypeId());
-    var sMapType;
-    // === IF Zoom Level <= 8 use mapStyleZoomedIn 
-    if (zoomLevel <= 8)
-        console.log("zoom");
-    // === If Zoom Level > 8 use mapStyleZoomedOut 
-    else
-        console.log("nozoom");
-});
-}
 async function initMap() {
     // Request needed libraries.
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    const center = { lat: 49.31424626956194, lng: 14.152326085413861 };
+    const center = { lat: 49.61424626956194, lng: 14.552326085413861 };
     const map = new Map(document.getElementById("map"), {
-        zoom: 10,
+        zoom: 9.4,
         center,
         mapId: "4504f8b37365c3d0",
     });
@@ -52,93 +39,94 @@ function buildContent(property) {
 
     content.classList.add("property");
     content.innerHTML = `
-        <div class="property_base">
-            <img class="rounded" style="height: 2rem; width: auto; " src="${property.imgSrc}" alt="Card image cap">
+        <div class="property_base" style="background-image: url('${property.imgSrc}') !important;">
+            <div class="details carded">
+                <p class="card-title">${property.nazev}<p>
+            </div>
         </div>
-        <div class="details card">
-          <img class="card-img-top" src="${property.imgSrc}" alt="Card image cap">
-          <div class="card-body">
-              <h5 class="card-title">${property.nazev}</h5>
-              <div class="card-text">
-                  <ul>
-                      <li><i>${property.description}</i></li>
-                      <li>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                              fill="#3acf87" class="bi bi-file-break m-2" viewBox="0 0 16 16">
-                              <path
-                                  d="M0 10.5a.5.5 0 0 1 .5-.5h15a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zM12 0H4a2 2 0 0 0-2 2v7h1V2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v7h1V2a2 2 0 0 0-2-2zm2 12h-1v2a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-2H2v2a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-2z" />
-                          </svg>
-                          ${property.panely} panelů
-                      </li>
-                      <li>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                              fill="#3acf87" class="bi bi-battery m-2" viewBox="0 0 16 16">
-                              <path
-                                  d="M0 6a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6zm2-1a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H2zm14 3a1.5 1.5 0 0 1-1.5 1.5v-3A1.5 1.5 0 0 1 16 8z" />
-                          </svg>
-                          ${property.vykon} kWp výkon
-                      </li>
-                  </ul>
-              </div>
-          </div>
       `;
     return content;
 }
 
 const properties = [
     {
-        nazev: "Fotovoltaika Smrkovice",
-        description: "FVE v Písku obsahuje 82ks střídačů SolarEdge SE25K a SE16K",
+        nazev: "FVE Smrkovice",
         imgSrc: "./img/references/panely.jpg",
-        vykon: 5.3,
-        panely: 12,
         position: {
-            lat: 49.31424626956194,
-            lng: 14.152326085413861,
+            lat: 49.280943827855914, 
+            lng: 14.15541410817076
         },
     },
     {
-        nazev: "Fotovoltaika Písek",
-        description: "FVE v Písku obsahuje 82ks střídačů SolarEdge SE25K a SE16K",
+        nazev: "FVE Olešná",
         imgSrc: "./img/references/hradiste.jpg",
-        vykon: 5.3,
-        panely: 12,
         position: {
-            lat: 49.41424626956194,
-            lng: 13.832326085413861,
+            lat: 49.444545490522, 
+            lng: 15.26523714202351
         },
     },
     {
-        nazev: "Fotovoltaika Lýska",
-        description: "FVE v Písku obsahuje 82ks střídačů SolarEdge SE25K a SE16K",
+        nazev: "FVE Malé Nepodřice",
+        imgSrc: "./img/references/panely1.jpg",
+        position: {
+            lat: 49.30397590374699, 
+            lng: 14.091418819949741
+        },
+    },
+    {
+        nazev: "FVE Ražice",
         imgSrc: "./img/references/panely2.jpg",
-        vykon: 5.3,
-        panely: 12,
         position: {
-            lat: 49.21338206154829,
-            lng: 14.189628372059293
+            lat: 49.24252262394634, 
+            lng: 14.099493826557135
         },
     },
     {
-        nazev: "Fotovoltaika Marek",
-        description: "FVE v Písku obsahuje 82ks střídačů SolarEdge SE25K a SE16K",
-        imgSrc: "./img/references/panely.jpg",
-        vykon: 5.3,
-        panely: 12,
-        position: {
-            lat: 49.41338206154829,
-            lng: 14.189628372059293
-        },
-    },
-    {
-        nazev: "Fotovoltaika Valek",
-        description: "FVE v Písku obsahuje 82ks střídačů SolarEdge SE25K a SE16K",
+        nazev: "FVE Semice",
         imgSrc: "./img/references/semice.png",
-        vykon: 5.3,
-        panely: 12,
         position: {
-            lat: 49.21338206154829,
-            lng: 14.229628372059293
+            lat: 49.28658814376823, 
+            lng: 14.176138380051448
+        },
+    },
+    {
+        nazev: "FVE Okružní",
+        imgSrc: "./img/references/okruznii.png",
+        position: {
+            lat: 49.99304198841903, 
+            lng: 14.408417052048355
+        },
+    },
+    {
+        nazev: "FVE Jihlava",
+        imgSrc: "./img/references/jihlava.jpg",
+        position: {
+            lat: 49.40162817696042, 
+            lng: 15.569273592003913
+        },
+    },
+    {
+        nazev: "FVE Kluky",
+        imgSrc: "./img/references/kluky.jpeg",
+        position: {
+            lat: 49.905022519841985, 
+            lng: 15.323096832800978
+        },
+    },
+    {
+        nazev: "FVE Semice",
+        imgSrc: "./img/references/semice1.jpg",
+        position: {
+            lat: 49.28658814376823,  
+            lng: 14.890468841730007,
+        },
+    },
+    {
+        nazev: "FVE Vlašim",
+        imgSrc: "./img/references/vlasim.jpg",
+        position: {
+            lat: 49.69325286556854, 
+            lng: 14.914820859838647
         },
     },
 ];
